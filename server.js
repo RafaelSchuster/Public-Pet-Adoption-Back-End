6976c3bebbd3cf649fed76da697f616581ac42ad
RAFAEL SCHUSTER <rsc1335@gmail.com>
2020-12-21 21:53:42 +0200

    Connected Front-Back Username Fetching Working

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
-const express = require('express')
-const app = express()
-const port = 5000
-const multer = require('multer')
-const fs = require('fs')
-const cors = require('cors')
-const user = require('./public/user.json')
+const express = require('express');
+const app = express();
+const port = 5000;
+const multer = require('multer');
+const fs = require('fs');
+const cors = require('cors');
+const user = require('./public/user.json');
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 
-app.get('/user', (req, res)=>{
-    res.send(user)
+app.get('/user', (req, res) => {
+    res.send(user);
 })
 
-app.post('/user', (req, res)=>{
+app.post('/user', (req, res) => {
     fs.writeFile('./public/user.json', JSON.stringify(req.body.post, null, 2), (err, data) => {
-        if (err) console.log('Errorrr')
+        if (err) console.log('Errorrr');
     })
-    console.log(req.body)
+    console.log(req.body);
 })
 
-app.listen(port, ()=>{
-    console.log('Running on Port 5000')
+app.listen(port, () => {
+    console.log('Running on Port 5000');
 })
