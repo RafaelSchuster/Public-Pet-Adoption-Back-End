7c9b3eb6a6dffcc3ed33a4266c9ffd6249fbf7b5
RAFAEL SCHUSTER <rsc1335@gmail.com>
2020-12-21 23:22:18 +0200

    Storing All Users Working

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const multer = require('multer');
 const fs = require('fs');
 const cors = require('cors');
 const user = require('./public/user.json');
+const allUsers = require('./public/AllUsers.json');
 
 app.use(express.json());
 app.use(cors());
@@ -15,10 +16,16 @@ app.get('/user', (req, res) => {
 })
 
 app.post('/user', (req, res) => {
+    const {firstName, lastName, telephone} = req.body.post
     fs.writeFile('./public/user.json', JSON.stringify(req.body.post, null, 2), (err, data) => {
-        if (err) console.log('Errorrr');
+        if (err) console.log('Error here');
     })
-    console.log(req.body);
+    console.log(req.body.post)
+    allUsers.push({'firstName' : firstName , 'lastName' : lastName, 'telephone' : telephone})
+    fs.writeFile('./public/AllUsers.json', JSON.stringify(allUsers, null, 2) ,(err, data)=>{
+        if(err)console.log('Error here')
+    } )
+    res.send('Updated');
 })
 
 app.listen(port, () => {
