7b34fdc31da272472d486a4ce516a3c5d045a18f
RAFAEL SCHUSTER <rsc1335@gmail.com>
2020-12-23 23:53:08 +0200

    Storing Imgs, Img Log, Allowing User and Pet Admin  Update

--- server.jsx
+++ server.jsx
@@ -4,15 +4,42 @@ const port = 5000;
 const multer = require('multer');
 const fs = require('fs');
 const cors = require('cors');
+const path = require('path')
 const allUsers = require('./public/AllUsers.json');
 const currentUser = require('./public/CurrentUser.json')
 const allPets = require('./public/AllPets.json')
 const userCount = require('./public/UserCount.json')
 const petCount = require('./public/PetCount.json')
-const {checkUser, checkById} = require('./checking')
+const images = require('./public/ImagesLog.json')
+const { checkUser, checkById } = require('./checking')
 app.use(express.json());
 app.use(cors());
 
+
+const storage = multer.diskStorage({
+    destination: './images',
+    filename: (req, file, cb) => {
+        const { id } = req.params
+        cb(null,`${id}- ${file.originalname} -${Math.random()}${path.extname(file.originalname)}`);
+    }
+});
+
+const upload = multer({ storage});
+app.use(express.static('images'));
+
+app.post('/image_upload/:id', upload.single('file'), (req, res, next) => {
+    const { id } = req.params
+    const logObj = {
+        'id': id,
+        "FileName": req.file.originalname,
+        "FilePath": req.file.path,   
+    }
+    images.push(logObj);
+    fs.writeFile('./public/ImagesLog.json', JSON.stringify(images, null, 2), (err, data) => {
+        if (err) console.log('Errorrr');
+    })
+    res.send('Done');
+});
 
 app.post('/user_sign', (req, res) => {
     console.log(userCount)
@@ -63,7 +90,7 @@ app.get('/userlogin', (req, res) => {
 app.post('/userprofile', (req, res) => {
     if (checkUser(req.body)) {
         const userIndex = checkUser(req.body);
-        allUsers[userIndex] = req.body.post;
+        allUsers[userIndex] = { ...allUsers[userIndex], ...req.body.post }
         fs.writeFile('./public/AllUsers.json', JSON.stringify(allUsers, null, 2), (err, data) => {
             if (err) console.log('Error in POST /userprofile');
         });
@@ -92,6 +119,7 @@ app.post('/pet_profile', (req, res) => {
         dietRestrictions,
         petBio,
         petStatus,
+        petsOwned,
         hypoalergenic
     } = req.body.post
 
@@ -105,8 +133,9 @@ app.post('/pet_profile', (req, res) => {
         'weight': weight,
         'dietRestrictions': dietRestrictions,
         'petBio': petBio,
-        'petStatus' : petStatus,
-        'hypoalergenic' : hypoalergenic
+        'petStatus': petStatus,
+        'petsOwned': petsOwned,
+        'hypoalergenic': hypoalergenic
 
     })
     fs.writeFile('./public/AllPets.json', JSON.stringify(allPets, null, 2), (err, data) => {
@@ -123,13 +152,40 @@ app.get('/allpets', (req, res) => {
 })
 
 app.get('/users/:id', (req, res) => {
-    const {id } = req.params
-    res.send(allUsers[checkById(allUsers,id)]) 
+    const { id } = req.params
+    res.send(allUsers[checkById(allUsers, id)])
 })
 app.get('/pets/:id', (req, res) => {
-    const {id } = req.params
-    res.send(allPets[checkById(allPets,id)]) 
+    const { id } = req.params
+    res.send(allPets[checkById(allPets, id)])
 })
+
+app.post('/user_admin_edit', (req, res) => {
+    const{id} = req.body.post
+    allUsers[checkById(allUsers, id)] = {...allUsers[checkById(allUsers, req.id)],...req.body.post}
+    fs.writeFile('./public/AllUsers.json', JSON.stringify(allUsers, null, 2), (err, data) => {
+        if (err) console.log('Error in POST /userprofile');
+    });
+    fs.writeFile('./public/CurrentUser.json', JSON.stringify(req.body.post, null, 2), (err, data) => {
+        if (err) console.log('Error here');
+    })
+    res.send('yes')
+})
+
+app.post('/pet_admin_edit', (req, res) => {
+    const{id} = req.body.post
+    allPets[checkById(allPets, id)] = {...allPets[checkById(allPets, req.id)],...req.body.post}
+    fs.writeFile('./public/AllPets.json', JSON.stringify(allPets, null, 2), (err, data) => {
+        if (err) console.log('Error in POST /petprofile');
+    });
+    res.send('yes')
+})
+
+app.get('/images/:id', (req, res) =>{
+    const {id} = req.params
+    
+})
+
 
 app.listen(port, () => {
     console.log('Running on Port 5000');
